8a28cabb732e11f27db11b1c44b250bc2411fd5d
Abdullah Aki <abdullahismailaki@gmail.com>
2022-04-09 21:41:37 +0300

    init commit
    Signed-off-by: Abdullah Aki <abdullahismailaki@gmail.com>

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/AbdullahAki/pilavli
+
+go 1.17
+
+require (
+	github.com/bwmarrin/discordgo v0.24.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	gorm.io/driver/sqlite v1.3.1 // indirect
+	gorm.io/gorm v1.23.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+type Users struct {
+	gorm.Model
+	UserID string
+}
+
+var db *gorm.DB
+
+func main() {
+	discord, err := discordgo.New("Bot " + os.Getenv("TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsDirectMessages
+
+	discord.AddHandler(messageCreate)
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	db, err = gorm.Open(sqlite.Open("pilavli.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Users{})
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!bilgilendir" {
+		fmt.Println(db.Where("user_id = ?", m.Author.ID).First(&Users{}).RowsAffected)
+		if db.Where("user_id = ?", m.Author.ID).First(&Users{}).RowsAffected == 0 {
+			channel, err := s.UserChannelCreate(m.Author.ID)
+			if err != nil {
+				fmt.Println("error creating channel:", err)
+				return
+			}
+			_, err = s.ChannelMessageSend(channel.ID, "Bildirimler basariyla acildi. Gelecek yayinlarda buradan bildirim alacaksin.")
+			if err != nil {
+				fmt.Println(err)
+				s.ChannelMessageSendReply(m.ChannelID, "DM'in kapali oldugu icin bildirimler acilamadi. DM'i acip tekrar !bilgilendir yazabilirsin.", &discordgo.MessageReference{MessageID: m.Message.ID, ChannelID: m.ChannelID, GuildID: m.GuildID})
+				return
+			} else {
+				db.Create(&Users{UserID: m.Author.ID})
+				s.MessageReactionAdd(m.ChannelID, m.Message.ID, "👍")
+				return
+			}
+		} else {
+			s.ChannelMessageSendReply(m.ChannelID, "Bildirimler daha onceden acilmis iptal etmek icin !bilgilendirme yazabilirsin.", &discordgo.MessageReference{MessageID: m.Message.ID, ChannelID: m.ChannelID, GuildID: m.GuildID})
+			return
+		}
+	}
+
+	if m.Content == "!bilgilendirme" {
+		fmt.Println(db.Where("user_id = ?", m.Author.ID).First(&Users{}).RowsAffected)
+		if db.Where("user_id = ?", m.Author.ID).First(&Users{}).RowsAffected > 0 {
+			db.Where("user_id = ?", m.Author.ID).Delete(&Users{})
+			s.MessageReactionAdd(m.ChannelID, m.Message.ID, "👍")
+			return
+		} else {
+			s.ChannelMessageSendReply(m.ChannelID, "Acik bildirim yok bildirimleri acmak icin !bilgilendir yazabilirsin.", &discordgo.MessageReference{MessageID: m.Message.ID, ChannelID: m.ChannelID, GuildID: m.GuildID})
+			return
+		}
+	}
+
+	if strings.Split(m.Content, " ")[0] == "!bilgiver" && m.Author.ID == "268062426312736770" {
+		var users []Users
+		db.Find(&users)
+		for _, d := range users {
+			channel, err := s.UserChannelCreate(d.UserID)
+			if err != nil {
+				fmt.Println("error creating channel:", err)
+				continue
+			}
+			_, err = s.ChannelMessageSend(channel.ID, strings.Join(strings.Split(m.Content, " ")[1:], " "))
+			if err != nil {
+				fmt.Println("error sending message:", err)
+				continue
+			}
+		}
+	}
+}
